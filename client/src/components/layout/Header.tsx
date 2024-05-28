8e95852c06c30c6a36790fcf5d49d3dd3499f4d8
ajmal-j <ajmaljaleel7860@gmail.com>
2024-05-29 03:37:25 +0530

    Update Header.tsx

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from "../buttons/theme-toggle";
 
 export default function Header() {
   return (
-    <div className='flex md:px-3 px-1 py-6 items-center justify-between bg-gradient-to-b from-background to-transparent'>
+    <div className='flex md:px-3 px-2 py-6 items-center justify-between bg-gradient-to-b from-background to-transparent'>
       <Image
         src='/title.svg'
         className='dark:invert'
