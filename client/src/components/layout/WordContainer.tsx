2547fda2cd2799dbe00fc80fdb6e3c3f84cf99fc
ajmal-j <ajmaljaleel7860@gmail.com>
2024-05-29 02:16:28 +0530

    Feat : update font.

--- client/src/components/layout/WordContainer.tsx
+++ client/src/components/layout/WordContainer.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function WordContainer({ children }: { children: ReactNode }) {
   return (
-    <div className='relative text-3xl leading-relaxed break-all mt-3'>
+    <div className='relative xl:text-4xl text-3xl leading-relaxed break-all mt-3 font-mono'>
       {children}
     </div>
   );
