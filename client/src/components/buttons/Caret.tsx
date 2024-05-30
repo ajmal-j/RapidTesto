1eee11f09007a713e13ed5c56329f558f18333da
ajmal-j <ajmaljaleel7860@gmail.com>
2024-05-30 23:09:43 +0530

    feat : update caret.

--- client/src/components/buttons/Caret.tsx
+++ client/src/components/buttons/Caret.tsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 export default function Caret({ isFinished }: { isFinished: boolean }) {
   if (isFinished) return null;
-  
+
   return (
     <motion.div
       aria-hidden={true}
@@ -10,7 +10,7 @@ export default function Caret({ isFinished }: { isFinished: boolean }) {
       animate={{ opacity: 0 }}
       exit={{ opacity: 1 }}
       transition={{ repeat: Infinity, duration: 0.9, ease: "easeInOut" }}
-      className='inline-block bg-primary/60 w-0.5 h-7 rounded-full mb-[-2px]'
+      className='inline-block bg-primary/60 w-0.5 h-12 md:mb-0 mb-[-2px] xl:h-10 rounded-full'
     />
   );
 }
