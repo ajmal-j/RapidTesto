12df3664224b9ef3271924104720f3d36725bfe0
ajmal-j <ajmaljaleel7860@gmail.com>
2024-05-30 23:24:22 +0530

    chore : update header title.

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,10 +1,13 @@
-import { signIn } from "@/auth";
 import getSession from "@/lib/getSession";
-import Image from "next/image";
 import Link from "next/link";
 import { AvatarButton } from "../buttons/Avatar";
 import { ThemeToggle } from "../buttons/theme-toggle";
 import { Button } from "../ui/button";
+import { Montserrat } from "next/font/google";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default async function Header() {
   const session = await getSession();
@@ -13,13 +16,16 @@ export default async function Header() {
   return (
     <div className='flex md:px-3 px-2 py-6 items-center justify-between bg-gradient-to-b from-background to-transparent'>
       <Link href='/'>
-        <Image
+        {/* <Image
           src='/title.svg'
           className='dark:invert'
           alt='logo'
           width={160}
           height={10}
-        />
+        /> */}
+        <h1 className={cn("text-3xl font-thin", montserrat.className)}>
+          Rapid Testo
+        </h1>
       </Link>
 
       <div className='flex gap-2 items-center'>
