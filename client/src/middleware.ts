0581d746ac43393adf847a46fcb7d8cbd8c41b4f
ajmal-j <ajmaljaleel7860@gmail.com>
2024-06-01 04:09:52 +0530

    Update middleware.ts

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -2,7 +2,9 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export const middleware = (request: NextRequest) => {
-  const authCookie = cookies().get("authjs.session-token");
+  const authCookie =
+    cookies().get("__Secure-authjs.session-token") ??
+    cookies().get("authjs.session-token");
 
   if (!authCookie || !authCookie.value) {
     return NextResponse.redirect(new URL(`/api/auth/signin`, request.url));
