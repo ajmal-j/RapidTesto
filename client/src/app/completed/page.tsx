abfd51e8a2527e10a8323b97374bdf4a5d8f736c
ajmal-j <ajmaljaleel7860@gmail.com>
2024-06-01 22:53:54 +0530

    feat : completed page pagination.

--- client/src/app/completed/page.tsx
+++ client/src/app/completed/page.tsx
@@ -14,12 +14,16 @@ import {
 } from "@/components/ui/accordion";
 import Results from "@/components/layout/Results";
 import { Button } from "@/components/ui/button";
-import { RotateCcw } from "lucide-react";
+import { ChevronLeft, ChevronRight, RotateCcw } from "lucide-react";
 import Link from "next/link";
 
-type Props = {};
+type Props = {
+  searchParams: {
+    p: string;
+  };
+};
 
-const page = async ({}: Props) => {
+const page = async ({ searchParams: { p = "1" } }: Props) => {
   const session = await getSession();
   const userId = session?.user?.id;
   if (!userId)
@@ -33,38 +37,57 @@ const page = async ({}: Props) => {
     include: { missedLetters: true; result: true };
   }>[] = [];
 
+  let totalDocuments = 0;
+  const doc = 5;
+
   try {
-    completed = await prisma.completed.findMany({
-      where: {
-        userId,
-      },
-      include: {
-        missedLetters: true,
-        result: true,
-      },
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
+    const [count, data] = await prisma.$transaction([
+      prisma.completed.count({
+        where: {
+          userId,
+        },
+      }),
+      prisma.completed.findMany({
+        skip: doc * Number(p) - doc,
+        take: doc,
+        where: {
+          userId,
+        },
+        include: {
+          missedLetters: true,
+          result: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      }),
+    ]);
+    completed = data;
+    totalDocuments = count;
   } catch (error) {
     console.log(error);
   }
+
+  const hasNext =
+    Number(p) >= 1 && totalDocuments - (doc * (Number(p) + 1) - doc) >= 0;
+
+  const hasPrev = Number(p) > 1;
 
   return (
     <Wrapper>
       <div className='flex flex-col gap-2 mb-10'>
         <h1 className='sm:text-4xl text-3xl mb-5'>Completed</h1>
-        <h5 className='text-muted-foreground'>Total : {completed.length}</h5>
+        <h5 className='text-muted-foreground'>Total : {totalDocuments}</h5>
         <div className='flex flex-col gap-3'>
           {completed.map((c, i) => (
-            <div key={i} className='flex flex-col gap-1 border px-3 rounded-xl'>
+            <div key={i} className='flex flex-col gap-1 border px-3 rounded-xl bg-foreground/5'>
               <Accordion type='single' collapsible>
                 <AccordionItem value='item-1'>
                   <div className='flex gap-2 items-center'>
                     <div className='w-full flex-1'>
                       <AccordionTrigger>
                         <span>
-                          {i + 1}. {c.createdAt.toDateString()}
+                          {c.createdAt.toDateString()}
                           <span className='text-muted-foreground px-2'>at</span>
                           {c.createdAt.toLocaleTimeString("en-IN", {
                             hour: "numeric",
@@ -100,6 +123,28 @@ const page = async ({}: Props) => {
               />
             </div>
           ))}
+          <div className='flex justify-end gap-2'>
+            <Link href={hasPrev ? `/completed/?p=${Number(p) - 1}` : "#"}>
+              <Button
+                variant={"outline"}
+                className='rounded-full'
+                size={"sm"}
+                disabled={!(Number(p) > 1)}
+              >
+                <ChevronLeft size={16} />
+              </Button>
+            </Link>
+            <Link href={hasNext ? `/completed/?p=${Number(p) + 1}` : "#"}>
+              <Button
+                variant={"outline"}
+                className='rounded-full'
+                size={"sm"}
+                disabled={!hasNext}
+              >
+                <ChevronRight size={16} />
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
     </Wrapper>
