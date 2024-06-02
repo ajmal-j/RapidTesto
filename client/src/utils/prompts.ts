ebb13d7f4915ef218f793eac4d52198e1bbdf17c
ajmal-j <ajmaljaleel7860@gmail.com>
2024-06-02 12:28:10 +0530

    chore : update boost accuracy prompt.

--- client/src/utils/prompts.ts
+++ client/src/utils/prompts.ts
@@ -5,7 +5,7 @@ export const GenerateWordsPrompt = ({
   prompt: string;
   words: number;
 }) => {
-  return `Generate paragraph with minimum ${words}-words using the following prompt: "${prompt}.". The paragraph you generate is for testing the typing speed and accuracy. It can be random and it must include contents mentioned above and the paragraph contains only number, english letters or special characters that is the keyboard. Also you can add spaces if needed.`;
+  return `Generate paragraph with a length of  ${words}-words using the following topic: "${prompt}.". The paragraph you generate is for testing the typing speed and accuracy. It can be random and it must include contents mentioned above and the paragraph contains only number, english letters or special characters that is the keyboard. Also you can add spaces if needed.`;
 };
 
 export const GeneratePrecessionPracticePrompt = ({
@@ -15,11 +15,9 @@ export const GeneratePrecessionPracticePrompt = ({
   letters: Record<string, number>[];
   wordsLength: number;
 }) => {
-  return `I will provide you with a list of missed letters and their frequencies from a typing test. Your task is to use these letters and make a paragraph by shuffling the letters in the list based on their frequency. It could look something like 'akb dd adfdh dkasdfdshfs dfsd' and add some spaces between words if needed. Missed letters: ${JSON.stringify(
+  return `I will provide you with a list of missed letters and their frequencies from a typing test. Your task is to use these letters and make a dummy paragraph by shuffling the letters in the list based on their frequency. It could look something like 'akbtg ddgg adfdh dkasdfdshfs dfsd' and add some spaces between words if needed. letters: ${JSON.stringify(
     letters
-  )}'.Only return the result text in the format of a string and the result string must have atleast a length of ${
-    wordsLength * 5
-  }`;
+  )}'.Only return the result text in the format of a string and the result string must have atleast a length of ${wordsLength} words.`;
 };
 
 export type PromptUtils = {
