d5467eaa14bf649d0245d78b6847d4a257c69e16
ajmal-j <ajmaljaleel7860@gmail.com>
2024-06-02 12:18:29 +0530

    feat : add tooltip to boost accuracy.

--- client/src/components/buttons/BoostAccuracy.tsx
+++ client/src/components/buttons/BoostAccuracy.tsx
@@ -5,6 +5,11 @@ import { Loader, Rocket } from "lucide-react";
 import { toast } from "sonner";
 import { Button } from "../ui/button";
 import { useState } from "react";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
 
 type Props = {
   setCustomWords: ({ words }: { words: string }) => void;
@@ -38,18 +43,30 @@ const BoostAccuracy = ({ setCustomWords, count }: Props) => {
   };
 
   return (
-    <Button
-      variant={"outline"}
-      onClick={handleBoostAccuracy}
-      className='gap-2 flex'
-    >
-      {loading ? (
-        <Loader size={16} className='animate-spin' />
-      ) : (
-        <Rocket size={16} />
-      )}
-      <span>Boost Accuracy</span>
-    </Button>
+    <HoverCard>
+      <HoverCardTrigger>
+        <Button
+          variant={"outline"}
+          onClick={handleBoostAccuracy}
+          className='gap-2 flex'
+        >
+          {loading ? (
+            <Loader size={16} className='animate-spin' />
+          ) : (
+            <Rocket size={16} />
+          )}
+          <span>Boost Accuracy</span>
+        </Button>
+      </HoverCardTrigger>
+      <HoverCardContent className='w-full max-w-[400px] '>
+        <p className='text-sm'>
+          <Rocket size={16} className='animate-bounce inline me-2' />
+          *Enhance accuracy by generating new word sets based on frequently
+          missed letters. Requires completion of at least 5 chapters for
+          analysis.
+        </p>
+      </HoverCardContent>
+    </HoverCard>
   );
 };
 
