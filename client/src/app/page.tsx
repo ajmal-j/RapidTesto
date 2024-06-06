f94495e80ce0ba16085f13aac9ff6de92a52ac33
ajmal-j <ajmaljaleel7860@gmail.com>
2024-06-06 12:32:41 +0530

    Update page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,11 @@
 import Main from "../components/layout/Main";
 
-export default async function Home({
+export default function Home({
   searchParams: { completed },
 }: {
   searchParams: {
     completed: string;
   };
 }) {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-
   return <Main completed={completed} />;
 }
